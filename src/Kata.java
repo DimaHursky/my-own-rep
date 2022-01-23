1dc8ba5c5eef12343e891eee580375d624c3c72f
DimaHursky <77124180+DimaHursky@users.noreply.github.com>
2022-01-23 23:17:37 +0200

    changes2

--- src/Kata.java
+++ src/Kata.java
@@ -3,7 +3,7 @@ public class Kata {
     public static double getVolumeOfCuboid(final double length, final double width, final double height) {
 
         /**
-         *changes
+         *changes22222
          */
         String wal = "TXT";
         return  length * width * height ;
