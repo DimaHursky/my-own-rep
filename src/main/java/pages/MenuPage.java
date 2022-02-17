fdb46caed5c55fcded16cd57cd7f2820df9c845a
DimaHursky <77124180+DimaHursky@users.noreply.github.com>
2022-02-17 20:35:09 +0200

    test

--- src/main/java/pages/MenuPage.java
+++ src/main/java/pages/MenuPage.java
@@ -20,42 +20,45 @@ public class MenuPage extends BasePage {
 //    private final By loginBtn = By.cssSelector
 //            ("div.MuiPaper-root.MuiDrawer-paper.MuiDrawer-paperAnchorLeft.MuiDrawer-paperAnchorDockedLeft.MuiPaper-elevation0 > ul:nth-child(5) > a:nth-child(1)");
 
-//    private final By signupBtn = By.xpath
-//            ("//span[contains(.,'Sign Up')]");
-    private final By signupBtn = By.cssSelector
-            ("div > ul:nth-child(5) > a:nth-child(2) > div.MuiListItemText-root > span");
+    private final By signupBtn = By.xpath
+            ("//span[contains(.,'Sign Up')]");
+//    private final By signupBtn = By.cssSelector
+//            ("div > ul:nth-child(5) > a:nth-child(2) > div.MuiListItemText-root > span");
 
-//    private final By mainBtn = By.xpath
-//            ("//span[contains(.,'Main')]");
-    private final By mainBtn = By.cssSelector
-            ("div > ul:nth-child(3) > a:nth-child(1) > div.MuiListItemText-root > span");
-
-//    private final By userMain = By.xpath
-//            ("//*[@id=\"root\"]/div/div[1]/div/div/ul[1]/a");
-    private final By userMain = By.cssSelector
-            ("div > ul:nth-child(3) > a:nth-child(1) > div.MuiListItemText-root > span");
-
-//    private final By userParsel = By.xpath
-//            ("//span[contains(.,'My Parcels')]");
-    private final By userParsel = By.cssSelector
-            ("div > ul:nth-child(3) > a.MuiButtonBase-root.MuiListItem-root.jss10.MuiListItem-gutters.MuiListItem-button.active > div.MuiListItemText-root > span");
+    private final By mainBtn = By.xpath
+            ("//span[contains(.,'Main')]");
+//    private final By mainBtn = By.cssSelector
+//            ("div > ul:nth-child(3) > a:nth-child(1) > div.MuiListItemText-root > span");
 
 
-//    private final By userSettings = By.xpath
-//            ("//span[contains(.,'Settings')]");
-    private final By userSettings = By.cssSelector
-            ("div.MuiPaper-root.MuiDrawer-paper.MuiDrawer-paperAnchorLeft.MuiDrawer-paperAnchorDockedLeft.MuiPaper-elevation0 > ul:nth-child(3) > a:nth-child(3)");
+    private final By userParsel = By.xpath
+            ("//span[contains(.,'My Parcels')]");
+//    private final By userParsel = By.cssSelector
+//            ("div > ul:nth-child(3) > a.MuiButtonBase-root.MuiListItem-root.jss10.MuiListItem-gutters.MuiListItem-button.active > div.MuiListItemText-root > span");
+
+
+    private final By userSettings = By.xpath
+            ("//span[contains(.,'Settings')]");
+ //   private final By userSettings = By.cssSelector
+ //           ("div.MuiPaper-root.MuiDrawer-paper.MuiDrawer-paperAnchorLeft.MuiDrawer-paperAnchorDockedLeft.MuiPaper-elevation0 > ul:nth-child(3) > a:nth-child(3)");
 
 
     private final By userLogOut = By.xpath
             ("//span[contains(.,'Log Out')]");
+
 
 //  It needs to be changed because there are problems with localization, or finds another element
 
 //  private final By userLogOut = By.cssSelector
 //            ("div > ul:nth-child(5) > a > div.MuiListItemText-root > span");
 
+    private final By burgerBtn = By.xpath
+            ("//button[@class='MuiButtonBase-root MuiIconButton-root']");
 
+    public HeaderPage clkBurgerBtn() {
+        waitElementIsVisible(burgerBtn).click();
+        return new HeaderPage(driver);
+    }
 
     public LoginPage clkLoginBtn() {
         waitElementIsVisible(loginBtn).click();
@@ -106,7 +109,7 @@ public class MenuPage extends BasePage {
     // ------------------------------------------------------
 
     public boolean isUserMainDsp() {
-        return waitElementIsVisible(userMain).isDisplayed();
+        return waitElementIsVisible(mainBtn).isDisplayed();
     }
     public boolean isUserParselDsp() {
         return waitElementIsVisible(userParsel).isDisplayed();
@@ -116,6 +119,9 @@ public class MenuPage extends BasePage {
     }
     public boolean isUserLogOutDsp() {
         return waitElementIsVisible(userLogOut).isDisplayed();
+    }
+    public boolean isBurgerBtnDsp() {
+        return waitElementIsVisible(burgerBtn).isDisplayed();
     }
 
     private final By tracksterHdng = By.xpath
--- src/main/java/pages/SignupPage.java
+++ src/main/java/pages/SignupPage.java
@@ -2,11 +2,52 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SignupPage extends BasePage {
     public SignupPage(WebDriver driver) {
         super(driver);
     }
+
+    private final By createYourAcc = By.cssSelector
+            ("h4.MuiTypography-root.MuiTypography-h4.MuiTypography-gutterBottom");
+    private final By fldEmail = By.id("email");
+    private final By fldPassword = By.id("pasword");
+    private final By fldConfirmPassword = By.id("password-confirmation");
+    private final By btnSighUp = By.cssSelector
+            ("button.MuiButtonBase-root.MuiButton-root.MuiButton-contained.MuiButton-containedPrimary.MuiButton-containedSizeLarge.MuiButton-sizeLarge.MuiButton-disableElevation");
+    private final By loginWithGoogle = By.cssSelector
+            ("a.MuiButtonBase-root.MuiButton-root.MuiButton-contained.MuiButton-containedSecondary.MuiButton-containedSizeLarge.MuiButton-sizeLarge.MuiButton-disableElevation.MuiButton-fullWidth  span.MuiButton-label span.MuiButton-startIcon.MuiButton-iconSizeLarge:nth-child(1)");
+    private final By loginWithFacebook = By.xpath
+            ("//span[contains(text(), 'Login with Facebook')]");
+    private final By logInUrl = By.xpath
+            ("//span//a[@href='/login']");
+    private final By massageInvalidEmail = By.xpath
+            ("//p[@id='email-helper-text']");
+
+    public SignupPage inputNewEmail(String newEmail, String password, String confirmPassword){
+        waitElementIsVisible(fldEmail).sendKeys(newEmail);
+        waitElementIsVisible(fldPassword).sendKeys(password);
+        waitElementIsVisible(fldConfirmPassword).sendKeys(confirmPassword);
+        WebElement btnSignUpClk = driver.findElement(btnSighUp);
+        btnSignUpClk.click();
+        return this;
+    }
+
+
+    /**
+     *  Methods find elements on page
+     */
+
+    public boolean findCreateYourAcc() {return driver.findElement(createYourAcc).isDisplayed();}
+    public boolean findEmailFld() {return driver.findElement(fldEmail).isDisplayed();}
+    public boolean findPasswordFld() {return driver.findElement(fldPassword).isDisplayed();}
+    public boolean findConfirmPasswordFld() {return driver.findElement(fldConfirmPassword).isDisplayed();}
+    public boolean findSignUpBtn() {return driver.findElement(btnSighUp).isDisplayed();}
+    public boolean findLoginWithGoogle() {return driver.findElement(loginWithGoogle).isDisplayed();}
+    public boolean findLoginWithFacebook() {return driver.findElement(loginWithFacebook).isDisplayed();}
+    public boolean findLogInUrl() {return driver.findElement(logInUrl).isDisplayed();}
+    public boolean findMassageInvalidEmail() {return driver.findElement(massageInvalidEmail).isDisplayed();}
 
     private final By signUpImg = By.cssSelector("div.MuiGrid-root.MuiGrid-container");
 
@@ -20,7 +61,7 @@ public class SignupPage extends BasePage {
 //        return new ForgotPasswordPage(driver);
 //    }
 
-    public boolean isSignUpImgspl() {
+    public boolean isSignUpImgDsp() {
         return driver.findElement(signUpImg).isDisplayed();
     }
 }
